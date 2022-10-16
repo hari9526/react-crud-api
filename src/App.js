7747f0037040b2e022abb2cb279b4d58b4f44fed
Harikrishnan <hari04.krishnan@gmail.com>
2022-10-16 22:57:47 +0530

    API call using fetch

--- src/App.js
+++ src/App.js
@@ -25,36 +25,88 @@ function App() {
 
 
   const getUserData = async () => {
-    await axios.get('http://localhost:4000/posts')
-      .then(res => {
-        console.log(res.data);
-        setData(res.data);
-      })
+    // await axios.get('http://localhost:4000/posts')
+    //   .then(res => {
+    //     console.log(res.data);
+    //     setData(res.data);
+    //   })
+    //   .catch(err => console.log(err));
+
+    //using fetch
+    await fetch('http://localhost:4000/posts')
+      .then((res) => res.json())
+      .then((data) => setData(data))
       .catch(err => console.log(err));
+
   };
 
   const handleDelete = async (id) => {
-    await axios.delete(`http://localhost:4000/posts/${id}`)
+    // await axios.delete(`http://localhost:4000/posts/${id}`)
+    //   .then(res => getUserData())
+    //   .then(err => console.log(err));
+    debugger; 
+
+    //fetch
+    await fetch(`http://localhost:4000/posts/${id}`, {
+      method: 'DELETE'
+    })
+
+      .then(res => res.json())
       .then(res => getUserData())
       .then(err => console.log(err));
+
+
   };
 
 
-  const handleFormSubmit = async (e) => {  
-    axios.post("http://localhost:4000/posts", formData)
-      .then(res => console.log(res))
+  const handleFormSubmit = async (e) => {
+    //post
+    // await axios.post("http://localhost:4000/posts", formData)
+    //   .then(res => console.log(res))
+    //   .catch(err => console.log(err));
+
+    //Fetch 
+
+    await fetch('http://localhost:4000/posts', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
       .catch(err => console.log(err));
+
     setFormData(initialData);
     getUserData();
+
+
   };
 
-  const handleEditData = () => {
+  const handleEditData = async () => {
     debugger;
-    axios.put(`http://localhost:4000/posts/${editData.id}`, editData)
-      .then(res => console.log(res))
+    // await axios.put(`http://localhost:4000/posts/${editData.id}`, editData)
+    //   .then(res => console.log(res))
+    //   .catch(err => console.log(err));
+
+    //fetch
+
+    await fetch(`http://localhost:4000/posts/${editData.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(editData),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
       .catch(err => console.log(err));
+
     setEditData(initialData);
     getUserData();
+
+
   };
 
   return (
@@ -78,7 +130,7 @@ function App() {
         handleFormSubmit={handleFormSubmit}
         editData={editData}
         setEditData={setEditData}
-        handleEditData = {handleEditData}
+        handleEditData={handleEditData}
       />
     </div>
   );
